ef93433b106ae6ad3e5649b6ce0f405223b6a149
brunaprogrammer <brunabiologarj@gmail.com>
2023-02-08 12:15:57 -0300

    sem chave

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,40 +6,20 @@ function App() {
   const [searchedCity, setSearchedCity] = useState('')
   const [city, setCity] = useState('')
   const [weather, setWeather] = useState(null)
-  const apiKey = "9a1c29c019c7d982a2ee1fb294d7e0ef"
+  const apiKey = ""
 
-  /* async function getCityWeather() {
-     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=metric&appid=${apiKey}&lang=pt_br`)
-     const data = await response.json()
-     setCity(searchedCity)
-     console.log(data)
-   }*/
 
   function handleSubmit(e) {
     e.preventDefault()
-    //getCityWeather()
     setCity(searchedCity)
-    console.log(searchedCity)
-
   }
 
   useEffect(() => {
     async function getCityWeather() {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchedCity}&units=metric&appid=${apiKey}&lang=pt_br`)
       const data = await response.json()
-      console.log("pingo")
       setWeather(data)
-
-      console.log(data)
-      console.log(data.weather[0].description)
-      console.log(data.main.temp)
-      console.log(weather.main.temp)
-
-
-
-      const temperatura = weather.main.temp;
     }
-
     getCityWeather()
   }, [city])
 
@@ -64,8 +44,7 @@ function App() {
   const mes = meses[date.getMonth()];
 
 
-
-  /*return (
+  return (
 
     <div className="App">
       <h1>{"Previsão do Tempo".toUpperCase()}</h1>
@@ -73,7 +52,6 @@ function App() {
         <form action="" className="wrapper--form" onSubmit={handleSubmit}>
           <input type="text" placeholder='Buscar por cidade' value={searchedCity} onChange={e => setSearchedCity(e.target.value)} />
           <button type='submit'>Procurar</button>
-
         </form>
       </div>
 
@@ -83,9 +61,6 @@ function App() {
           <h2>{city}</h2>
 
         </>)}
-
-
-
 
       {weather && weather.main && (
         <>
@@ -98,31 +73,15 @@ function App() {
               <h2>{weather.main.temp.toFixed(0)}°C</h2>
               <p className='descricao'>{weather.weather[0].description}</p>
             </div>
-
             <p className='max'>Máxima</p>
           </div>
-
         </>)}
-
-
-
-
-
-
-
-
     </div>
+  )
 
-
-  )*/
-
-  return (
+  /*return (
     <Routes />
-  );
-
-
-
-
+  );*/
 }
 
 export default App
